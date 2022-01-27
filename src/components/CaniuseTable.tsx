3af8f28871e839e15398fe74b44456ee10971f9e
Clark Alesna <clark_alesna@hotmail.com>
2022-01-27 22:26:50 +0800

    partial mobile support (need more improvements)

--- src/components/CaniuseTable.tsx
+++ src/components/CaniuseTable.tsx
@@ -12,40 +12,45 @@ export default function CaniuseTable(props: {
   api: Api
 }): JSX.Element {
   const walletCompatibility = getCompatbility(props.api);
-  return <TableContainer component={TablePaper}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          {walletCompatibility.map((row, i) => {
-            return <TableCell align="center" key={i}>
-              <img width="20" height="20" src={row.logo} alt={row.name} style={{
-                marginRight: "8px",
-                transform: "translateY(4px)"
-              }} />
-              <span>{row.name}</span>
-            </TableCell>
-          })}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        <TableRow
-          key='1'
-          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-        >
-          {walletCompatibility.map((row, i) => {
-            switch (row.support) {
-              case SupportLevel.Supported:
-                return <SupportedCell key={i}>SUPPORTED</SupportedCell>
-              case SupportLevel.Experimental:
-                return <PartialSupportedCell key={i}>EXPERIMENTAL SUPPORT</PartialSupportedCell>
-              case SupportLevel.Unsupported:
-                return <NotSupportedCell key={i}>NOT SUPPORTED</NotSupportedCell>
-              default:
-                return <UnknownCell key={i}>UNKNOWN</UnknownCell>
-            }
-          })}
-        </TableRow>
-      </TableBody>
-    </Table>
-  </TableContainer>
+  return <div style={{
+    width: "100%",
+    overflowX: "auto"
+  }}>
+    <TableContainer component={TablePaper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {walletCompatibility.map((row, i) => {
+              return <TableCell align="center" key={i}>
+                <img width="20" height="20" src={row.logo} alt={row.name} style={{
+                  marginRight: "8px",
+                  transform: "translateY(4px)"
+                }} />
+                <span>{row.name}</span>
+              </TableCell>
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow
+            key='1'
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+          >
+            {walletCompatibility.map((row, i) => {
+              switch (row.support) {
+                case SupportLevel.Supported:
+                  return <SupportedCell key={i}>SUPPORTED</SupportedCell>
+                case SupportLevel.Experimental:
+                  return <PartialSupportedCell key={i}>EXPERIMENTAL SUPPORT</PartialSupportedCell>
+                case SupportLevel.Unsupported:
+                  return <NotSupportedCell key={i}>NOT SUPPORTED</NotSupportedCell>
+                default:
+                  return <UnknownCell key={i}>UNKNOWN</UnknownCell>
+              }
+            })}
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </div>
 }
