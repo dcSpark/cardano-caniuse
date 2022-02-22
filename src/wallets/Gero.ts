bf46668381742b4354ccfac1addb5a5e77d76ba1
John Maverick <johnmaverick76@gmail.com>
2022-02-22 23:04:17 +0200

    Add documentation for GeroWallet

--- src/wallets/Gero.ts
+++ src/wallets/Gero.ts
@@ -3,9 +3,28 @@ import { Api, SupportLevel } from "../system/Types";
 export default {
   name: 'Gero',
   injectAs: 'gerowallet',
-  logo: '',
+  logo: 'https://gerowallet.io/assets/img/logo2.ico',
   Apis: {
+    [Api.Enable]: SupportLevel.Supported,
+    [Api.IsEnabled]: SupportLevel.Supported,
+    [Api.ApiVersion]: SupportLevel.Supported,
+    [Api.Name]: SupportLevel.Supported,
+    [Api.Icon]: SupportLevel.Supported,
+    [Api.GetNetworkId]: SupportLevel.Supported,
+    [Api.GetUtxos]: SupportLevel.Supported,
+    [Api.GetBalance]: SupportLevel.Supported,
     [Api.GetUsedAddresses]: SupportLevel.Supported,
-    [Api.GetUtxos]: SupportLevel.Unsupported
+    [Api.GetUnusedAddresses]: SupportLevel.Supported,
+    [Api.GetChangeAddress]: SupportLevel.Supported,
+    [Api.GetRewardAddress]: SupportLevel.Supported,
+    [Api.GetRewardAddresses]: SupportLevel.Unsupported,
+    [Api.SignTx]: SupportLevel.Supported,
+    [Api.SignData]: SupportLevel.Supported,
+    [Api.SubmitTx]: SupportLevel.Supported,
+    [Api.GetCollateral]: SupportLevel.Experimental,
+    [Api.On]: SupportLevel.Unsupported,
+    [Api.Off]: SupportLevel.Unsupported,
+    [Api.OnAccountChange]: SupportLevel.Unsupported,
+    [Api.OnNetworkChange]: SupportLevel.Unsupported
   }
 }
