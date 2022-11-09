48a467df6aea7ab7ec37006ebcb3fc7b76db37ce
Clark Alesna <clark_alesna@hotmail.com>
2022-11-09 22:55:46 +0800

    Updated CC wallet to Eternl

--- src/Wallets.ts
+++ src/Wallets.ts
@@ -1,14 +1,14 @@
 import Flint from "./wallets/Flint";
 import Gero from "./wallets/Gero";
 import Nami from "./wallets/Nami";
-import Cv from "./wallets/Cv";
+import Eternl from "./wallets/Eternl";
 import Nufi from "./wallets/Nufi";
 import Begin from "./wallets/Begin";
 
 export const Wallets = [
   Flint,
   Nami,
-  Cv, 
+  Eternl, 
   Gero,
   Nufi,
   Begin,
