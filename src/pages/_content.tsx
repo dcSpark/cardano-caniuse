0c18aeec1303d0576a70183be2a79a2930932e0a
Clark Alesna <clark_alesna@hotmail.com>
2022-11-14 03:32:24 +0800

    Updated Docusaurus version to 2.2.0 | NOT BETA ANYMORE 🚀 (#12)
    * Updated CC wallet to Eternl
    
    * Updated to latest docusaurus 2.2.0
    
    * renamed content.tsx to _content.tsx to stop docusaurus from treating it like a page instead of a compoment
    
    * Updated NPM dependency versions
    
    * remove console.log
    
    * removed unused import useIsBrowser
    
    * Updated `typescript` and `@tsconfig/docusauru`

---
+++ src/pages/_content.tsx
@@ -0,0 +1,59 @@
+import React, { useMemo } from 'react';
+import clsx from 'clsx';
+import Head from '@docusaurus/Head';
+import styles from './index.module.css';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useColorMode } from '@docusaurus/theme-common';
+import { createTheme, Paper, ThemeProvider, useTheme } from '@mui/material';
+import Matrix from '../components/Matrix';
+
+function HomepageHeader() {
+    const { siteConfig } = useDocusaurusContext();
+    const theme = useTheme();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)} style={{
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.getContrastText(theme.palette.primary.main),
+        }}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+            </div>
+        </header>
+    );
+}
+
+export default function ThemeContainer(props): JSX.Element {
+    // Docusaurus Theme
+    let { colorMode } = useColorMode();
+
+    // Material UI Theme
+    const reactTheme = useMemo(() => {
+        return createTheme({
+            palette: {
+                mode: colorMode,
+            },
+        });
+    }, [colorMode]);
+
+    return (
+        <ThemeProvider theme={reactTheme}>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                />
+            </Head>
+            <HomepageHeader />
+            <main>
+                <Paper className={styles.main}>
+                    <Matrix />
+                </Paper>
+            </main>
+        </ThemeProvider>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,13 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import Head from '@docusaurus/Head';
-import { Paper, useTheme } from '@mui/material';
-import Matrix from '../components/Matrix';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  const theme = useTheme();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)} style={{
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.getContrastText(theme.palette.primary.main),
-      }}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import Content from './_content';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  const theme = useTheme();
-
   return (
     <Layout
       title=''
       description="Up-to-date wallet support tables for cip30 (dApp connector) support">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
-      </Head>
-      <HomepageHeader />
-      <main>
-        <Paper className={styles.main}>
-          <Matrix />
-        </Paper>
-      </main>
+      <Content />
     </Layout>
   );
 }
